493ac2220dc89fa48619ebc5d2937a770fd93f80
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-23 10:11:10 -0600

    Update paymentservertests.h

--- src/qt/test/paymentservertests.h
+++ src/qt/test/paymentservertests.h
@@ -10,26 +10,5 @@
 #include <QObject>
 #include <QTest>
 
-class PaymentServerTests : public QObject
-{
-    Q_OBJECT
-
-private slots:
-    void paymentServerTests();
-};
-
-// Dummy class to receive paymentserver signals.
-// If SendCoinsRecipient was a proper QObject, then
-// we could use QSignalSpy... but it's not.
-class RecipientCatcher : public QObject
-{
-    Q_OBJECT
-
-public slots:
-    void getRecipient(SendCoinsRecipient r);
-
-public:
-    SendCoinsRecipient recipient;
-};
 
 #endif // BITCOIN_QT_TEST_PAYMENTSERVERTESTS_H

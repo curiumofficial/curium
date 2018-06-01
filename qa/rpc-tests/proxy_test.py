5014ca0e4b3237052769f6b20ecd5724423c3f32
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-31 21:08:40 -0600

    changed port

--- qa/rpc-tests/proxy_test.py
+++ qa/rpc-tests/proxy_test.py
@@ -100,12 +100,12 @@ class ProxyTest(BitcoinTestFramework):
         rv.append(cmd)
 
         # Test: outgoing onion connection through node
-        node.addnode("curiumvj7kcklujarx.onion:11771", "onetry")
+        node.addnode("curiumvj7kcklujarx.onion:18745", "onetry")
         cmd = proxies[2].queue.get()
         assert(isinstance(cmd, Socks5Command))
         assert_equal(cmd.atyp, AddressType.DOMAINNAME)
         assert_equal(cmd.addr, "curiumvj7kcklujarx.onion")
-        assert_equal(cmd.port, 11771)
+        assert_equal(cmd.port, 18745)
         if not auth:
             assert_equal(cmd.username, None)
             assert_equal(cmd.password, None)
--- src/qt/curiumstrings.cpp
+++ src/qt/curiumstrings.cpp
@@ -226,10 +226,10 @@ QT_TRANSLATE_NOOP("curium-core", ""
 QT_TRANSLATE_NOOP("curium-core", ""
 "You must specify a masternodeprivkey in the configuration. Please see "
 "documentation for help."),
-QT_TRANSLATE_NOOP("curium-core", "(11771 could be used only on mainnet)"),
+QT_TRANSLATE_NOOP("curium-core", "(18745 could be used only on mainnet)"),
 QT_TRANSLATE_NOOP("curium-core", "(default: %s)"),
 QT_TRANSLATE_NOOP("curium-core", "(default: 1)"),
-QT_TRANSLATE_NOOP("curium-core", "(must be 11771 for mainnet)"),
+QT_TRANSLATE_NOOP("curium-core", "(must be 18745 for mainnet)"),
 QT_TRANSLATE_NOOP("curium-core", "<category> can be:"),
 QT_TRANSLATE_NOOP("curium-core", "Accept command line and JSON-RPC commands"),
 QT_TRANSLATE_NOOP("curium-core", "Accept connections from outside (default: 1 if no -proxy or -connect)"),
